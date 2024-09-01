56b6e4488c39ff95b444c446c814cb5b866db7e0
yassinekrika <yassinekrika2016@gmail.com>
2024-09-01 09:06:52 +0100

    source write fixing

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -7,17 +7,38 @@ export const DATA = {
   url: "https://yassg4mer.space",
   location: "Jijel, Algeria",
   locationLink: "",
-  description: "Software Engineer who loves building innovative solutions and empowering others through technology.",
-  summary: '[Computer science engineer](/#education) specializing in full-stack web development, with expertise in API development using [Node Js and Express Js](/#skills). Skilled in crafting interactive, data-driven dashboards with [Angular and Next Js](/#skills), dedicated to delivering impactful solutions and enhancing user engagement.',
+  description:
+    "Software Engineer who loves building innovative solutions and empowering others through technology.",
+  summary:
+    "[Computer science engineer](/#education) specializing in full-stack web development, with expertise in API development using [Node Js and Express Js](/#skills). Skilled in crafting interactive, data-driven dashboards with [Angular and Next Js](/#skills), dedicated to delivering impactful solutions and enhancing user engagement.",
   avatarUrl: "/me.jpeg",
   skills: [
-    'TypeScript', 'JavaScript', 'Python', 'Solidity',
-    'HTML5', 'CSS3', 'SCSS', 'Git', 'Tailwind Css', 'Angular', 'React', 'Next Js', 'Hardhat',
-    'Node Js', 'Express Js',
-    'MySql', 'Mongodb', 'Postgresql', 'Firebase', 'Oracle',
-    'Linux', 'Github', 'GCP',
-    'Figma', 'Ui Design',
-    'Problem Solving',
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "Solidity",
+    "HTML5",
+    "CSS3",
+    "SCSS",
+    "Git",
+    "Tailwind Css",
+    "Angular",
+    "React",
+    "Next Js",
+    "Hardhat",
+    "Node Js",
+    "Express Js",
+    "MySql",
+    "Mongodb",
+    "Postgresql",
+    "Firebase",
+    "Oracle",
+    "Linux",
+    "Github",
+    "GCP",
+    "Figma",
+    "Ui Design",
+    "Problem Solving",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -64,27 +85,29 @@ export const DATA = {
       logoUrl: "/weewee-logo.svg",
       start: "October 2023",
       end: "Present",
-      description: `As a web developer in my role, i specialize in crafting interactive and data-driven dashboards using Angular and Firebase. my mission is to transform complex data into intuitive user experiences, leveraging real-time updates for dynamic content. i am dedicated to pushing the boundaries of web development to create impactful solutions that drive results and elevate user engagement, currently i am working on :\n
+      description:
+        `As a web developer in my role, i specialize in crafting interactive and data-driven dashboards using Angular and Firebase. my mission is to transform complex data into intuitive user experiences, leveraging real-time updates for dynamic content. i am dedicated to pushing the boundaries of web development to create impactful solutions that drive results and elevate user engagement, currently i am working on :\n
           , Helping in developing new logistics solution\n
           , Developing web solution for logistics i helped in launching two of our main services this year (Grand Public + WeePicks)
           , Developing the WeeWee dashboard using Angular framework and it's core features
           , Developing the web version of the WeeWee client applications\n
-          , Working on developing WeeWee API and leveraging other companies APIs using Node.js and Express.js server with Typescript and Swagger for documentation`
+          , Working on developing WeeWee API and leveraging other companies APIs using Node.js and Express.js server with Typescript and Swagger for documentation`,
     },
     {
       company: "Self Employed",
-      badges: ['Freelance'],
+      badges: ["Freelance"],
       href: "/",
       location: "Remote",
       title: "Full-stack Developer",
       logoUrl: "/freelance.jpeg",
       start: "Jun 2023",
       end: "Present",
-      description: "Developed and maintained a variety of websites, APIs using Express Js, and applications using React and Next Js according to client needs. Improved performance, security, and user engagement for multiple projects."
+      description:
+        "Developed and maintained a variety of websites, APIs using Express Js, and applications using React and Next Js according to client needs. Improved performance, security, and user engagement for multiple projects.",
     },
     {
       company: "Private Schools",
-      badges: ['Ellabs', 'Skill House School'],
+      badges: ["Ellabs", "Skill House School"],
       href: "/",
       location: "Remote",
       title: "Course Instructor",
@@ -93,7 +116,7 @@ export const DATA = {
       end: "Fabrury 2024",
       description: `
       Course instructor at ELLabs teaching the basic fundamental of linux and Git and Github, with 90% students improved in team contribution to projects.
-      Course instructor at Skill House in Fullstack web development , teaching basic fundamental of front-end web development Html, Css, JavaSript, as well as back-end using Node Js, Express Js, Mysql, in database.`
+      Course instructor at Skill House in Fullstack web development , teaching basic fundamental of front-end web development Html, Css, JavaSript, as well as back-end using Node Js, Express Js, Mysql, in database.`,
     },
   ],
   education: [
@@ -120,14 +143,15 @@ export const DATA = {
       href: "/",
       dates: "October 2023 - Present",
       active: true,
-      description: 'I developed an admin dashboard using Nebular and Firebase at WeeWee Delivery. The project involved creating a responsive UI for seamless data management and real-time analytics. Firebase was used for data storage, real-time updates, and authentication, ensuring a scalable and user-friendly solution for the company\'s operational needs.',
+      description:
+        "I developed an admin dashboard using Nebular and Firebase at WeeWee Delivery. The project involved creating a responsive UI for seamless data management and real-time analytics. Firebase was used for data storage, real-time updates, and authentication, ensuring a scalable and user-friendly solution for the company's operational needs.",
       technologies: [
         "Angular",
         "Typescript",
         "Html",
         "Scss",
         "Nebular Ui Library",
-        "Firebase"
+        "Firebase",
       ],
       links: [
         {
@@ -149,7 +173,8 @@ export const DATA = {
       href: "/",
       dates: "January 2024 - Jun 2024",
       active: true,
-      description: "Excited that we have developed a Blockchain-based system for healthcare management on Ethereum platform using both blockchain development technologies and web development technologies, as a tech stack we used Hardhart as a development environment for creating, testing, deploying Solidity smart contract and Next Js for frontend of the application, paving the way for innovative solutions in the industry.",
+      description:
+        "Excited that we have developed a Blockchain-based system for healthcare management on Ethereum platform using both blockchain development technologies and web development technologies, as a tech stack we used Hardhart as a development environment for creating, testing, deploying Solidity smart contract and Next Js for frontend of the application, paving the way for innovative solutions in the industry.",
       technologies: [
         "Next.js",
         "Typescript",
@@ -157,7 +182,7 @@ export const DATA = {
         "Solidity",
         "HardHat",
         "Remix",
-        "Shadcn Ui"
+        "Shadcn Ui",
       ],
       links: [],
       image: "",
@@ -169,20 +194,21 @@ export const DATA = {
       dates: "August 2023",
       active: true,
       description:
-          "Designed, developed a web tools that contains a collection of handy web tools that can simplify various tasks for you. Below, you'll find information about each tool and how to use them..",
+        "Designed, developed a web tools that contains a collection of handy web tools that can simplify various tasks for you. Below, you'll find information about each tool and how to use them..",
       technologies: [
         "Next.js",
         "Typescript",
         "TailwindCSS",
         "Html",
         "Css",
-        "JavaScript"
+        "JavaScript",
       ],
       links: [
         {
           type: "Figma",
-          href: "https://www.figma.com/design/UyTe8OBlIbN2209VPdrjEv/Blogger-Tool?m=auto&t=RroTdGHXFZavE3u1-6",
-          icon: <Icons.figma className="size-3" />
+          href:
+            "https://www.figma.com/design/UyTe8OBlIbN2209VPdrjEv/Blogger-Tool?m=auto&t=RroTdGHXFZavE3u1-6",
+          icon: <Icons.figma className="size-3" />,
         },
         {
           type: "Website",
@@ -203,13 +229,14 @@ export const DATA = {
       href: "https://main--vermillion-praline-4b0207.netlify.app/",
       dates: "Jun 2023",
       active: true,
-      description: 'Siplify your reading with Summzie, an open-source article summarizer that transforms lengthy articles into clear and concise summaries',
+      description:
+        "Siplify your reading with Summzie, an open-source article summarizer that transforms lengthy articles into clear and concise summaries",
       technologies: [
         "React",
         "Vite",
         "Html",
         "Css",
-        "JavaScript"
+        "JavaScript",
       ],
       links: [
         {
@@ -231,19 +258,20 @@ export const DATA = {
       href: "https://ecommerce-three-tan.vercel.app/",
       dates: "Jun 2023",
       active: true,
-      description: 'Design, developed a ecommerce website clone',
+      description: "Design, developed a ecommerce website clone",
       technologies: [
         "Next.js",
         "JavaScript",
         "Html",
         "Css",
         "Prisma",
-        "MySql"
+        "MySql",
       ],
       links: [
         {
           type: "Figma",
-          href: "https://www.figma.com/design/KXcFnAHMNCdZlfMAxZPIjV/e-commerce-wesite?m=auto&t=RroTdGHXFZavE3u1-6",
+          href:
+            "https://www.figma.com/design/KXcFnAHMNCdZlfMAxZPIjV/e-commerce-wesite?m=auto&t=RroTdGHXFZavE3u1-6",
           icon: <Icons.figma className="size-3" />,
         },
         {
@@ -265,7 +293,8 @@ export const DATA = {
       href: "/",
       dates: "October 2022",
       active: true,
-      description: 'We developed an open-source online application that tracks the space station and accurately represents the Earth, the orbit of the station, and the station itself in three dimensions.',
+      description:
+        "We developed an open-source online application that tracks the space station and accurately represents the Earth, the orbit of the station, and the station itself in three dimensions.",
       technologies: [
         "Web Pack",
         "JavaScript",
@@ -275,7 +304,7 @@ export const DATA = {
       ],
       links: [
         {
-          type: "Source",
+          type: "Youtube",
           href: "https://www.youtube.com/watch?v=i2-8PUHjtaU",
           icon: <Icons.youtube className="size-3" />,
         },
@@ -293,26 +322,27 @@ export const DATA = {
       href: "/",
       dates: "October 2022",
       active: true,
-      description: 'We developed an Android application and a chrome extension powered with Chat gpt Api to response automatically for emails and a voice chatbot to let users ineract with the Ai.',
+      description:
+        "We developed an Android application and a chrome extension powered with Chat gpt Api to response automatically for emails and a voice chatbot to let users ineract with the Ai.",
       technologies: [
-          "Flutter",
-          "Html",
-          "Css",
-          "JavaScript",
-          "Chrome Extension",
-          "Figma",
+        "Flutter",
+        "Html",
+        "Css",
+        "JavaScript",
+        "Chrome Extension",
+        "Figma",
       ],
       links: [
         {
           type: "Figma",
-          href: "https://www.figma.com/design/q9oAiowFNE29ml6r3M7664/junction-app?m=auto&t=RroTdGHXFZavE3u1-6",
+          href:
+            "https://www.figma.com/design/q9oAiowFNE29ml6r3M7664/junction-app?m=auto&t=RroTdGHXFZavE3u1-6",
           icon: <Icons.figma className="size-3" />,
-        }
+        },
       ],
       image: "",
       video: "/video/junction.mp4",
     },
-
   ],
   hackathons: [
     {
@@ -321,8 +351,7 @@ export const DATA = {
       location: "Alger, Algeria",
       description: `
           We developed a school management dashboard that manages student and courses they take also a calendar management.`,
-      image:
-          "/hackwave-hackathon.png",
+      image: "/hackwave-hackathon.png",
       links: [
         {
           title: "Github",
@@ -337,13 +366,13 @@ export const DATA = {
       location: "Alger, Algeria",
       description: `
           We developed an Android application and a chrome extension powered with Chat gpt Api to response automatically for emails and a voice chatbot to let users ineract with the Ai.`,
-      image:
-          "/junction-hackathon.png",
+      image: "/junction-hackathon.png",
       links: [
         {
           title: "Figma",
           icon: <Icons.figma className="h-4 w-4" />,
-          href: "https://www.figma.com/design/q9oAiowFNE29ml6r3M7664/junction-app?m=auto&t=RroTdGHXFZavE3u1-6",
+          href:
+            "https://www.figma.com/design/q9oAiowFNE29ml6r3M7664/junction-app?m=auto&t=RroTdGHXFZavE3u1-6",
         },
       ],
     },
@@ -353,8 +382,7 @@ export const DATA = {
       location: "Alger, Algeria",
       description: `
           We developed an open-source online application that tracks the space station and accurately represents the Earth, the orbit of the station, and the station itself in three dimensions.`,
-      image:
-        "/nasa-hackathon.jpeg",
+      image: "/nasa-hackathon.jpeg",
       links: [
         {
           title: "Github",
